f803104df550442b6ec43976d74f1d4efa3a5b4c
hjtunen <91310858+hjtunen@users.noreply.github.com>
2021-11-09 13:35:31 +0200

    3.4

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
@@ -81,7 +81,7 @@ app.post('/api/persons', (request, response) => {
     id: generateId(),
   }
 
-  notes = notes.concat(person)
+  persons = persons.concat(person)
 
   response.json(person)
 })
