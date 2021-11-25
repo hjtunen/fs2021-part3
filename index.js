a99999ba25c6e63b5ef9c639a204c7d7ce80070e
hjtunen <91310858+hjtunen@users.noreply.github.com>
2021-11-25 14:12:38 +0200

    3.16

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 require("dotenv").config()
 const Person = require("./models/person")
-const mongoose = require('mongoose')
 
 /* let persons = [
   { 
@@ -52,25 +51,26 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  if (mongoose.isValidObjectId(request.params.id)) {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+  
+  Person.findById(request.params.id)
+    .then(person => {
       if (person) {
         response.json(person)
       }else {
         response.status(404).end()
       }
     })
-  }else {
-    response.status(404).end()
-  }
+    .catch(error => next(error))
+  
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -105,6 +105,24 @@ app.post('/api/persons', (request, response) => {
   })
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
